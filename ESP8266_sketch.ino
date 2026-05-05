006beba37d84612447348d374b46fae175adc6f4
CHM <132416621+Chisyoso@users.noreply.github.com>
2026-05-05 11:34:15 -0600

    Resolve string literal errors and update WiFi config
    Fix compilation errors related to string literals and update WiFi credentials.

--- ESP8266_sketch.ino
+++ ESP8266_sketch.ino
@@ -1,125 +1,85 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino: In function 'void sendJson(bool, const String&)':
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:26:22: error: unable to find string literal operator 'operator""ok' with 'const char [3]', 'unsigned int' arguments
+   26 |   String json = "{"ok":";
+      |                      ^~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino: In function 'void handlePing()':
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:37:24: error: unable to find string literal operator 'operator""pong' with 'const char [6]', 'unsigned int' arguments
+   37 |   sendJson(true, ""pong":true");
+      |                        ^~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino: In function 'void handleCmd()':
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:42:28: error: inconsistent user-defined literal suffixes 'error' and 'missing' in string literal
+   42 |     sendJson(false, ""error":"missing c"");
+      |                            ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:42:28: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:49:26: error: unable to find string literal operator 'operator""pong' with 'const char [6]', 'unsigned int' arguments
+   49 |     sendJson(true, ""pong":true");
+      |                          ^~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:55:59: error: inconsistent user-defined literal suffixes 'error' and 'invalid' in string literal
+   55 |     if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
+      |                                                           ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:55:59: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:64:59: error: inconsistent user-defined literal suffixes 'error' and 'invalid' in string literal
+   64 |     if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
+      |                                                           ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:64:59: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:72:50: error: inconsistent user-defined literal suffixes 'error' and 'bad' in string literal
+   72 |     if (first < 0) return sendJson(false, ""error":"bad format"");
+      |                                                  ^~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:72:50: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:75:59: error: inconsistent user-defined literal suffixes 'error' and 'invalid' in string literal
+   75 |     if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
+      |                                                           ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:75:59: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:78:25: error: unable to find string literal operator 'operator""pin' with 'const char [2]', 'unsigned int' arguments
+   78 |     sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
+      |                         ^~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:78:51: error: unable to find string literal operator 'operator""val' with 'const char [3]', 'unsigned int' arguments
+   78 |     sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
+      |                                                   ^~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:84:59: error: inconsistent user-defined literal suffixes 'error' and 'invalid' in string literal
+   84 |     if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
+      |                                                           ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:84:59: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:87:25: error: unable to find string literal operator 'operator""pin' with 'const char [2]', 'unsigned int' arguments
+   87 |     sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
+      |                         ^~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:87:51: error: unable to find string literal operator 'operator""val' with 'const char [3]', 'unsigned int' arguments
+   87 |     sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
+      |                                                   ^~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:93:47: error: inconsistent user-defined literal suffixes 'error' and 'bad' in string literal
+   93 |     if (p1 < 0) return sendJson(false, ""error":"bad format"");
+      |                                               ^~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:93:47: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:96:59: error: inconsistent user-defined literal suffixes 'error' and 'invalid' in string literal
+   96 |     if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
+      |                                                           ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:96:59: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:100:40: error: inconsistent user-defined literal suffixes 'error' and 'bad' in string literal
+  100 |     else return sendJson(false, ""error":"bad mode"");
+      |                                        ^~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:100:40: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:105:26: error: inconsistent user-defined literal suffixes 'error' and 'unknown' in string literal
+  105 |   sendJson(false, ""error":"unknown command"");
+      |                          ^~~~~~~~~~
+C:\Users\jesus\Documents\midi\Zoe___Labios_Rotos__MIDIfind\Zoe___Labios_Rotos__MIDIfind.ino:105:26: error: unable to find string literal operator 'operator""error' with 'const char [2]', 'unsigned int' arguments
+exit status 1
 
-const char* ssid = "TU_WIFI";
-const char* password = "TU_PASSWORD";
+Compilation error: unable to find string literal operator 'operator""ok' with 'const char [3]', 'unsigned int' arguments
 
-ESP8266WebServer server(80);
 
-const int allowedPins[] = {0, 2, 4, 5, 12, 13, 14, 15, 16};
+usa siempre estas variables de ip 
 
-bool isAllowedPin(int pin) {
-  for (int p : allowedPins) if (p == pin) return true;
-  return false;
-}
+const char* ssid = "Familia_Zambrano";
+const char* password = "20082018";
+ 
+añade esto como verificador
 
-void cors() {
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-  server.sendHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  server.sendHeader("Access-Control-Allow-Headers", "Content-Type");
-}
-
-void sendJson(bool ok, const String& extra = "") {
-  cors();
-  String json = "{"ok":";
-  json += ok ? "true" : "false";
-  if (extra.length()) {
-    json += ",";
-    json += extra;
-  }
-  json += "}";
-  server.send(200, "application/json", json);
-}
-
-void handlePing() {
-  sendJson(true, ""pong":true");
-}
-
-void handleCmd() {
-  if (!server.hasArg("c")) {
-    sendJson(false, ""error":"missing c"");
-    return;
-  }
-
-  String cmd = server.arg("c");
-
-  if (cmd == "ping") {
-    sendJson(true, ""pong":true");
-    return;
-  }
-
-  if (cmd.startsWith("PIN_OUT:")) {
-    int pin = cmd.substring(8).toInt();
-    if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, LOW);
-    sendJson(true);
-    return;
-  }
-
-  if (cmd.startsWith("PIN_IN:")) {
-    int pin = cmd.substring(7).toInt();
-    if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
-    pinMode(pin, INPUT);
-    sendJson(true);
-    return;
-  }
-
-  if (cmd.startsWith("PIN_WRITE:")) {
-    int first = cmd.indexOf(':', 10);
-    if (first < 0) return sendJson(false, ""error":"bad format"");
-    int pin = cmd.substring(10, first).toInt();
-    int val = cmd.substring(first + 1).toInt();
-    if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, val ? HIGH : LOW);
-    sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
-    return;
-  }
-
-  if (cmd.startsWith("PIN_READ:")) {
-    int pin = cmd.substring(9).toInt();
-    if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
-    pinMode(pin, INPUT);
-    int val = digitalRead(pin);
-    sendJson(true, ""pin":" + String(pin) + ","val":" + String(val));
-    return;
-  }
-
-  if (cmd.startsWith("PIN_MODE:")) {
-    int p1 = cmd.indexOf(':', 9);
-    if (p1 < 0) return sendJson(false, ""error":"bad format"");
-    int pin = cmd.substring(9, p1).toInt();
-    String mode = cmd.substring(p1 + 1);
-    if (!isAllowedPin(pin)) return sendJson(false, ""error":"invalid pin"");
-    if (mode == "OUTPUT") pinMode(pin, OUTPUT);
-    else if (mode == "INPUT") pinMode(pin, INPUT);
-    else if (mode == "INPUT_PULLUP") pinMode(pin, INPUT_PULLUP);
-    else return sendJson(false, ""error":"bad mode"");
-    sendJson(true);
-    return;
-  }
-
-  sendJson(false, ""error":"unknown command"");
-}
-
-void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
+ while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
 
-  server.on("/ping", HTTP_GET, handlePing);
-  server.on("/cmd", HTTP_OPTIONS, []() { cors(); server.send(204); });
-  server.on("/cmd", HTTP_GET, handleCmd);
-  server.begin();
-
+  Serial.println("\nConectado!");
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
-}
-
-void loop() {
-  server.handleClient();
-}
+ 
